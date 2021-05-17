8aa4333e8f5f22d0be7de406150f99a70a193dee
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-05-17 17:49:07 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity='go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (adjective='*'){
+   const innerFunction = function(value){
+       return `You are ${adjective}${value}${adjective}!`
+   }
+return innerFunction
+}
+
